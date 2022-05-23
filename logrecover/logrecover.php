e3ee9c5a45711d8a68afee86d7df50054ca9408b
satopian <satopian@mia.eek.jp>
2022-05-23 19:10:21 +0900

    ログ修復ツール。個別スレッドのログファイルから全体ログを再構築します。

---
+++ logrecover/logrecover.php
@@ -0,0 +1,24 @@
+<?php
+//全体ログを個別スレッドのログをもとに修復
+//作成されるログファイルの名前はnew_alllog.log
+$logfiles_arr =(glob('./log/*.log', GLOB_BRACE));//ログファイルをglob
+
+	$arr_logs=[];
+	$logs=[];
+	foreach($logfiles_arr as $i=>$logfile){//ログファイルを一つずつ開いて読み込む
+	if(strpos($logfile,'alllog')===false){
+		$fp=fopen($logfile,"r+");
+		$line =fgets($fp);
+		if(trim($line)){
+			list($r_no,$oyasub,$n_,$v_,$c_,$u_,$img_,$_,$_,$thumb_,$pt_,$md5_,$to_,$pch_,$postedtime,$fp_time_,$h_,$uid_,$h_,$r_oya)=explode("\t",trim($line));
+			if($r_oya==='oya'){
+				$logs[$r_no]=$line;//1スレッド分
+			}
+		}
+		fclose($fp);
+	}
+	}
+	krsort($logs);
+	$logs=array_values($logs);
+file_put_contents('./log/new_alllog.log',$logs);
+echo '再構築が完了しました。<br>new_alllog.logという仮のファイル名で再構築したログファイルを出力しました。';
