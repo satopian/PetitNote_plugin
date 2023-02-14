0ee82d8f69c559394b24f7a3af3f62cbe0ed4784
satopian <satopian@mia.eek.jp>
2023-02-14 14:13:42 +0900

    ページングを実装。

--- search/search.php
+++ search/search.php
@@ -1,15 +1,15 @@
 <?php
-$max_search=180;
+$max_search=300;
 
 //設定の読み込み
-require(__DIR__.'/config.php');
-const JQUERY ='jquery-3.6.0.min.js';
-$en= lang_en();
+require_once(__DIR__.'/config.php');
 
-if($err=check_file(__DIR__.'/lib/'.JQUERY)){
-	die($err);
-}
-$jquery=JQUERY;
+$lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
+  ? explode( ',', $http_langs )[0] : '';
+$en= (stripos($lang,'ja')!==0);
+
+$jquery='jquery-3.6.0.min.js';
+check_file(__DIR__.'/lib/'.$jquery);
 
 //HTMLテンプレート
 
@@ -19,7 +19,6 @@ $skindir = 'template/'.$skindir;
 
 $imgsearch=(bool)filter_input(INPUT_GET,'imgsearch',FILTER_VALIDATE_BOOLEAN);
 $page=(int)filter_input(INPUT_GET,'page',FILTER_VALIDATE_INT);
-$page= $page ? $page : 1;
 $query=filter_input(INPUT_GET,'query');
 $query=urldecode($query);
 $query=mb_convert_kana($query, 'rn', 'UTF-8');
@@ -29,7 +28,7 @@ $query=h($query);
 $radio =filter_input(INPUT_GET,'radio',FILTER_VALIDATE_INT);
 
 if($imgsearch){
-	$disp_count_of_page=30;//画像検索の時の1ページあたりの表示件数
+	$disp_count_of_page=20;//画像検索の時の1ページあたりの表示件数
 }
 else{
 	$disp_count_of_page=30;//通常検索の時の1ページあたりの表示件数
@@ -77,32 +76,29 @@ while ($log = fgets($fp)) {
 						$query!==''&&($radio===1||$radio===null)&&stripos($s_name,$query)===0||//作者名が含まれる
 						$query!==''&&($radio===2&&$s_name===$query)//作者名完全一致
 				){
-					$link='';
-					$link='./?res='.$no.'#'.$time;
-					$arr[]=[$no,$name,$sub,$com,$imgfile,$w,$h,$time,$link];
+					$arr[]=[$no,$name,$sub,$com,$imgfile,$w,$h,$time];
 					++$i;
+					if($i>=$max_search){break 2;}//1掲示板あたりの最大検索数
 				}
-					if($i>=$max_search){break;}//1掲示板あたりの最大検索数
 				
 			}
 
-			if($j>=5000){break;}//1掲示板あたりの最大行数
-			++$j;
 
 		}
 		fclose($cp);
 	}
+	if($j>=5000){break;}//1掲示板あたりの最大行数
+	++$j;
 }
 fclose($fp);
-
 
 //検索結果の出力
 $j=0;
 $comments=[];
 if($arr){
 	foreach($arr as $i => $val){
-		if($i > $page-2){//$iが表示するページになるまで待つ
-			list($no,$name,$sub,$com,$imgfile,$w,$h,$time,$link)=$val;
+		if($i >= $page){//$iが表示するページになるまで待つ
+			list($no,$name,$sub,$com,$imgfile,$w,$h,$time)=$val;
 			$img='';
 			if($imgfile){
 				if(is_file(THUMB_DIR.$time.'s.jpg')){//サムネイルはあるか？
@@ -113,7 +109,7 @@ if($arr){
 					}
 				}
 
-			$time=(strlen($time)>15) ? substr($time,0,-6) : substr($time,0,-3);
+			$postedtime=(strlen($time)>15) ? substr($time,0,-6) : substr($time,0,-3);
 
 			$postedtime =$time ? (date("Y/m/d G:i", (int)$time)) : '';
 			$sub=h($sub);
@@ -126,15 +122,16 @@ if($arr){
 			$name=h($name);
 			$encoded_name=urlencode($name);
 			//変数格納
-			$comments[]= compact('no','name','encoded_name','sub','img','w','h','com','link','postedtime');
+			$comments[]= compact('no','name','encoded_name','sub','img','w','h','com','time','postedtime');
 
 		}
 			$j=$i+1;//表示件数
-			if($i >= $page+$disp_count_of_page-2){break;}
+			if($i >= $page+$disp_count_of_page-1){break;}
 	}
 }
 unset($sub,$name,$no);
 unset($i,$val);
+// var_dump($comments);
 
 $search_type='';
 if($imgsearch){
@@ -176,7 +173,7 @@ $page=(int)$page;
 
 $pageno='';
 if($j&&$page>=2){
-	$pageno = $page.'-'.$j.$mai_or_ken;
+	$pageno = ($page+1).'-'.$j.$mai_or_ken;
 }
 elseif($j){
 		$pageno = $j.$mai_or_ken;
@@ -202,36 +199,29 @@ $prevpage=$page-$disp_count_of_page;//前のページ
 $countarr=count($arr);//配列の数
 $prev=false;
 $next=false;
-$next_txt='';
-$prev_txt='';
-if($page<=$disp_count_of_page){
-	$prev_txt=$en ? "Return to bulletin board" : "掲示板にもどる";
-	$prev="./";//前のページ
-if($countarr>=$nextpage){
-	$next_txt=($en ? "next " :"次の").h($disp_count_of_page.$mai_or_ken).'≫';//次のページ
-	$next=$nextpage.$search_type.$query_l;//次のページ
-}
-}
 
-elseif($page>=$disp_count_of_page+1){
-	$prev_txt= "≪".($en ? "prev " : "前の").$disp_count_of_page.$mai_or_ken; 
-	$prev= $prevpage.$search_type.$query_l; 
-	if($countarr>=$nextpage){
-		$next_txt=($en ? "next " :"次の").$disp_count_of_page.$mai_or_ken."≫";
-		$next=h($nextpage.$search_type.$query_l);
-	}
-	else{
-		$prev_txt=$en ? "Return to bulletin board" : "掲示板にもどる";
-		$prev="./";
-	}
+//
+$countarr=count($arr);//配列の数
+
+//ページング
+$pagedef=$disp_count_of_page;
+$start_page=$page-$pagedef*8;
+$end_page=$page+($pagedef*8);
+if($page<$pagedef*17){
+	$start_page=0;
+	$end_page=$pagedef*17;
 }
+//prev next 
+$next=(($page+$pagedef)<$countarr) ? $page+$pagedef : false;//ページ番号がmaxを超える時はnextのリンクを出さない
+$prev=((int)$page<=0) ? false : ($page-$pagedef) ;//ページ番号が0の時はprevのリンクを出さない
+
 
 
 //最終更新日時を取得
 $postedtime='';
 $lastmodified='';
 if(!empty($arr)){
-	list($no,$name,$sub,$com,$imgfile,$w,$h,$postedtime,$link)=$arr[0];
+	list($no,$name,$sub,$com,$imgfile,$w,$h,$postedtime)=$arr[0];
 
 	$postedtime=(strlen($postedtime)>15) ? substr($postedtime,0,-6) : substr($postedtime,0,-3);
 	$lastmodified=date("Y/m/d G:i", (int)$postedtime);
@@ -239,35 +229,33 @@ if(!empty($arr)){
 
 unset($arr);
 
-function h($str){
-	return htmlspecialchars($str,ENT_QUOTES,'utf-8',false);
-}
-function lang_en(){//言語が日本語以外ならtrue。
-	$lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
-	? explode( ',', $http_langs )[0] : '';
-  return (stripos($lang,'ja')!==0);
-  
+
+// ファイル存在チェック
+function check_file ($path) {
+	$msg=initial_error_message();
+
+	if (!is_file($path)) return die(h($path) . $msg['001']);
+	if (!is_readable($path)) return die(h($path) . $msg['002']);
 }
 function initial_error_message(){
-	$en=lang_en();
+	global $en;
 	$msg['001']=$en ? ' does not exist.':'がありません。'; 
 	$msg['002']=$en ? ' is not readable.':'を読めません。'; 
-	$msg['003']=$en ? ' is not writable.':'に書けません。'; 
+	$msg['003']=$en ? ' is not writable.':'を書けません。'; 
 return $msg;	
 }
-// ファイル存在チェック
-function check_file ($path,$check_writable='') {
-	$msg=initial_error_message();
-	if (!is_file($path)) return $path . $msg['001']."<br>";
-	if (!is_readable($path)) return $path . $msg['002']."<br>";
-	if($check_writable){//書き込みが必要なファイルのチェック
-		if (!is_writable($path)) return $path . $msg['003']."<br>";
+
+//エスケープ
+function h($str){
+	if($str===0 || $str==='0'){
+		return '0';
 	}
-	return '';
+	if(!$str){
+		return '';
+	}
+	return htmlspecialchars($str,ENT_QUOTES,"utf-8",false);
 }
 
 //HTML出力
 $templete='search.html';
 return include __DIR__.'/'.$skindir.$templete;
-
-
